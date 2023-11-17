4750f9241eb7484cd804881db02b81be282efd68
Eren OĞUZ <92570103+Trustedaid@users.noreply.github.com>
2023-11-17 18:32:53 +0300

    Dynamic Infrastructure Implementation

--- Application/Features/Models/Profiles/MappingProfiles.cs
+++ Application/Features/Models/Profiles/MappingProfiles.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Features.Models.Queries.GetList;
+using Application.Features.Models.Queries.GetListByDynamic;
 using AutoMapper;
 using Core.Application.Responses;
 using Core.Persistence.Paging;
@@ -6,12 +7,35 @@ using Domain.Entities;
 
 namespace Application.Features.Models.Profiles;
 
-public class MappingProfiles : Profile
+/*
+ public class MappingProfiles : Profile
 {
     public MappingProfiles()
     {
         CreateMap<Model, GetListModelListItemDto>().ReverseMap();
         CreateMap<Paginate<Model>, GetListResponse<GetListModelListItemDto>>().ReverseMap();
+    }
+}
+*/
 
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<Model, GetListModelListItemDto>()
+            .ForMember(destinationMember: x => x.BrandName, memberOptions: x => x.MapFrom(x => x.Brand.Name))
+            .ForMember(destinationMember: x => x.FuelName, memberOptions: x => x.MapFrom(x => x.Fuel.Name))
+            .ForMember(destinationMember: x => x.TransmissionName,
+                memberOptions: x => x.MapFrom(x => x.Transmission.Name))
+            .ReverseMap();
+        CreateMap<Model, GetListByDynamicModelListItemDto>()
+            .ForMember(destinationMember: x => x.BrandName, memberOptions: x => x.MapFrom(x => x.Brand.Name))
+            .ForMember(destinationMember: x => x.FuelName, memberOptions: x => x.MapFrom(x => x.Fuel.Name))
+            .ForMember(destinationMember: x => x.TransmissionName,
+                memberOptions: x => x.MapFrom(x => x.Transmission.Name))
+            .ReverseMap();
+
+        CreateMap<Paginate<Model>, GetListResponse<GetListModelListItemDto>>().ReverseMap();
+        CreateMap<Paginate<Model>, GetListResponse<GetListByDynamicModelListItemDto>>().ReverseMap();
     }
 }
