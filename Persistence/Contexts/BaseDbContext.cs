c9925e527498ab319fb4defa730714de555abf15
Eren OĞUZ <92570103+Trustedaid@users.noreply.github.com>
2024-02-10 18:27:28 +0300

    EntityConfigurations

--- Persistence/Contexts/BaseDbContext.cs
+++ Persistence/Contexts/BaseDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using Core.Security.Entities;
 using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -13,7 +14,12 @@ public class BaseDbContext : DbContext
     public DbSet<Fuel> Fuels { get; set; }
     public DbSet<Model> Models { get; set; }
     public DbSet<Transmission> Transmissions { get; set; }
-    
+    public DbSet<OperationClaim> OperationClaims { get; set; }
+    public DbSet<OtpAuthenticator> OtpAuthenticators { get; set; }
+    public DbSet<RefreshToken> RefreshTokens { get; set; } 
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+    public DbSet<EmailAuthenticator> EmailAuthenticators { get; set; }
 
     public BaseDbContext(DbContextOptions<BaseDbContext> dbContextOptions, IConfiguration configuration) : base(
         dbContextOptions)
