2b26d1866bd47dcb50aec6b55320055cc53c8d35
Eren OĞUZ <92570103+Trustedaid@users.noreply.github.com>
2024-02-11 01:15:52 +0300

    Repository Configurations

--- Persistence/PersistenceServiceRegistration.cs
+++ Persistence/PersistenceServiceRegistration.cs
@@ -14,10 +14,19 @@ public static class PersistenceServiceRegistration
     {
         //services.AddDbContext<BaseDbContext>(options => options.UseInMemoryDatabase("nArchitecture"));
 
-        services.AddDbContext<BaseDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("RentACar")));
+        services.AddDbContext<BaseDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("RentACar")));
 
         services.AddScoped<IBrandRepository, BrandRepository>();
         services.AddScoped<IModelRepository, ModelRepository>();
+
+        services.AddScoped<IEmailAuthenticatorRepository, EmailAuthenticatorRepository>();
+        services.AddScoped<IOperationClaimRepository, OperationClaimRepository>();
+        services.AddScoped<IOtpAuthenticatorRepository, OtpAuthenticatorRepository>();
+        services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IUserOperationClaimRepository, UserOperationClaimRepository>();
+
         return services;
     }
 }
---
+++ Persistence/Repositories/RefreshTokenRepository.cs
@@ -0,0 +1,24 @@
+﻿using Application.Services.Repositories;
+using Core.Persistence.Repositories;
+using Core.Security.Entities;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Contexts;
+
+namespace Persistence.Repositories;
+
+public class RefreshTokenRepository : EfRepositoryBase<RefreshToken, int, BaseDbContext>, IRefreshTokenRepository
+{
+    public RefreshTokenRepository(BaseDbContext context) : base(context)
+    {
+    }
+
+    public async Task<List<RefreshToken>> GetOldRefreshTokenAsync(int userId, int refreshTokenTTL)
+    {
+        List<RefreshToken> tokens = await Query()
+            .AsNoTracking()
+            .Where(rt =>
+                rt.UserId == userId && rt.Revoked == null && rt.Expires >= DateTime.UtcNow &&
+                rt.CreatedDate.AddDays(refreshTokenTTL) <= DateTime.UtcNow).ToListAsync();
+        return tokens;
+    }
+}
